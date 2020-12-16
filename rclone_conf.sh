6253bd4c0fe328646ae50bcd0db16d52072b8cbe
sauravdharwadkar <dharwadkarsaurav@gmail.com>
2020-12-16 13:01:37 +0530

    unique key generation every time

--- rclone_conf.sh
+++ rclone_conf.sh
@@ -1,5 +1,6 @@
 # !/bin/bash
 echo -e "\nBASE_CONF\nNote: copy as it is without spaces \n\n\n"
+echo -e "#$(date -u)\n$(cat ~/.config/rclone/rclone.conf)" > ~/.config/rclone/rclone.conf
 cat ~/.config/rclone/rclone.conf | base64 --wrap=0 
 
 echo -e "\n\n\nhere CLOUD_NAME igone other token texts \n"
