58fb843f71ea07ba59d2c5c22ab7487e1db7617a
Cremesis <Cremesis@users.noreply.github.com>
2021-03-03 18:12:39 +0100

    Correct typo in rclone_conf.sh

--- rclone_conf.sh
+++ rclone_conf.sh
@@ -3,6 +3,6 @@ echo -e "\nBASE_CONF\nNote: copy as it is without spaces \n\n\n"
 echo -e "#$(date -u)\n$(cat ~/.config/rclone/rclone.conf)" > ~/.config/rclone/rclone.conf
 cat ~/.config/rclone/rclone.conf | base64 --wrap=0 
 
-echo -e "\n\n\nhere CLOUD_NAME igone other token texts \n"
+echo -e "\n\n\nhere CLOUD_NAME ignore other token texts \n"
 cat ~/.config/rclone/rclone.conf | grep "\[*\]"
 echo -e "\n"
